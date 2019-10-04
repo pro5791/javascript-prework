728b3a1874fcbdc26ad773d0e6ba209ae64c2258
Przemek Rogala <przemek@rogala.priv.pl>
2019-10-04 20:19:40 +0200

    Add code to script.js (1.2.3)

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1) {
+    computerMove = 'kamień';
+}    else if(randomNumber == 2) {
+    computerMove = 'papier';
+}    else if(randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
