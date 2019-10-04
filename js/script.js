85fb55cd995e8a7878219ac44784f5d16273abeb
Przemek Rogala <przemek@rogala.priv.pl>
2019-10-04 21:12:40 +0200

    Add code to script.js - results

--- js/script.js
+++ js/script.js
@@ -27,6 +27,37 @@ if (playerInput == '1'){
     playerMove = 'papier';
 }   else if (playerInput == '3'){
     playerMove = 'nożyce';
+}   else if (playerInput > '3'){
+    playerMove = 'nieznany ruch';
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Remis :)');
+}   else if ( computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+}   else if ( computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Komputer wygrywa!');
+}   else if( computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Komputer wygrywa!');
+}   else if ( computerMove == 'papier' && playerMove == 'papier') {
+    printMessage('Remis :)');
+}   else if ( computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+}   else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+}   else if ( computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Komputer wygrywa!');
+}   else if ( computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis :)');
+}   else if ( computerMove == 'nożyce' && playerMove == 'nożyce') {
+    printMessage('Remis :)');
+}   else if ( computerMove == 'kamień' && playerMove == 'nieznany ruch') {
+    printMessage('Inna liczba!? Wybierz liczbę 1: kamień, 2: papier, 3: nożyce');
+}   else if ( computerMove == 'papier' && playerMove == 'nieznany ruch') {
+    printMessage('Inna liczba!? Wybierz liczbę 1: kamień, 2: papier, 3: nożyce');
+}   else if ( computerMove == 'nożyce' && playerMove == 'nieznany ruch') {
+    printMessage('Inna liczba!? Wybierz liczbę 1: kamień, 2: papier, 3: nożyce');
+}
+
